89880118ae111517b064bfa4710b2f2630af2052
Márton Hartvig <34651552+hartvigmarton@users.noreply.github.com>
2024-07-30 20:12:50 +0200

    Added listing of all blog entries

--- articles/urls.py
+++ articles/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path('<int:post_id>/', views.blog_post_detail, name='blog_post_detail'),
     path('graph/', views.graph_view, name='graph_view'),
     path('graph3/',views.index_graph,name='index-graph'),
     path('blog/<int:post_id>/', views.blog_post_detail, name='blog_post_detail'),
